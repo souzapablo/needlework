e8b2af8c747334b63d8c4148f934c2d8c54ffc6e
Pablo Souza <pablo.osouza@outlook.com>
2023-05-16 08:11:05 -0300

    feat: create first controller and configure swagger

---
+++ src/NeedleWork.API/Controllers/SupplierController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NeedleWork.API.Controllers;
+
+[ApiController]
+[Route("api/v1/suppliers")]
+public class SupplierController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok();
+    }
+
+    [HttpGet("{id:long}")]
+    public IActionResult GetById(long id)
+    {
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok();
+    }
+
+    [HttpPut("{id:long}")]
+    public IActionResult Update(long id)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("{id:long}")]
+    public IActionResult Delete(long id) 
+    {
+        return Ok();
+    }
+}
--- src/NeedleWork.API/Program.cs
+++ src/NeedleWork.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "NeedleWork",
+        Version = "v1",
+        Description = "Register what you sell and what you buy so you can keep track of your money flow with no surprises at the end.",
+        Contact = new OpenApiContact
+        {
+            Name = "Pablo Souza",
+            Email = "pablo.osouza@outlook.com",
+            Url = new Uri("https://github.com/souzapablo")
+        }
+    });
+});
 
 var app = builder.Build();
 
