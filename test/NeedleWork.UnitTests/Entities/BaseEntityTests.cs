e69cdbed3e1d771da98f44b2c4e9de838777e256
Pablo Souza <pablo.osouza@outlook.com>
2023-08-27 08:50:12 -0300

    feat: delete entities

---
+++ test/NeedleWork.UnitTests/Entities/BaseEntityTests.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+
+namespace NeedleWork.UnitTests.Entities;
+
+public class BaseEntityTests
+{
+    [Fact]
+    [DisplayName("Given a deletion entities should change IsActive status to false")]
+    public void GivenDeleteRequestBaseEntityShouldChangeIsActiveStatus() 
+    {
+        // Given
+        Product product = new(1, "Test product", 2M, UnitOfMeasurement.Meter);
+
+        // When
+        product.Delete();
+
+        // Then
+        product.IsActive.Should().BeFalse();
+    }   
+}
--- test/NeedleWork.UnitTests/Usings.cs
+++ test/NeedleWork.UnitTests/Usings.cs
@@ -1,1 +1,4 @@
+global using FluentAssertions;
+global using NeedleWork.Core.Entities;
+global using NeedleWork.Core.Enums;
 global using Xunit;
