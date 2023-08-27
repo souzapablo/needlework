8e603b32a93df3312f480b4221de2abe076ecd81
Pablo Souza <pablo.osouza@outlook.com>
2023-08-27 08:50:33 -0300

    feat: delete entities

--- src/NeedleWork.Core/Entities/Shared/BaseEntity.cs
+++ src/NeedleWork.Core/Entities/Shared/BaseEntity.cs
@@ -6,4 +6,6 @@ public abstract class BaseEntity
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public DateTime LastUpdate { get; private set; } = DateTime.UtcNow;
     public bool IsActive { get; private set; } = true;
+
+    public void Delete() => IsActive = false;
 }
