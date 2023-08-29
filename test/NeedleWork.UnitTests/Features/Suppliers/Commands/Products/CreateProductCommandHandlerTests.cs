b84878ef2d7d6fc83956d516b2ecf6769477f7c8
Pablo Souza <oliveirasouzapablo@gmail.com>
2023-08-29 08:17:04 -0300

    test: cover create product command handler tests

---
+++ test/NeedleWork.UnitTests/Features/Suppliers/Commands/Products/CreateProductCommandHandlerTests.cs
@@ -0,0 +1,51 @@
+using NeedleWork.Application.Features.Products.Create;
+using NeedleWork.Core.Exceptions;
+
+namespace NeedleWork.UnitTests.Features.Suppliers.Commands.Products;
+
+public class CreateProductCommandHandlerTests
+{
+    private readonly IProductRepository _productRepository = Substitute.For<IProductRepository>();
+    private readonly ISupplierRepository _supplierRepository = Substitute.For<ISupplierRepository>();
+
+    [Fact]
+    [DisplayName("Given a valid input should create product")]
+    public async Task GivenAValidInputShouldCreateProduct()
+    {
+        // Given
+        CreateProductCommand command = new(1, "Test description", 20M, UnitOfMeasurement.Meter);
+        CreateProductCommandHander commandHandler = new(_productRepository, _supplierRepository);
+
+        _supplierRepository.VerifyIfExists(Arg.Any<long>())
+            .Returns(true);
+        
+        // When
+        await commandHandler.Handle(command, new CancellationToken());
+
+        // Then
+        await _supplierRepository.Received(1)
+            .VerifyIfExists(Arg.Any<long>());
+        await _productRepository.Received(1)
+            .CreateAsync(Arg.Any<Product>());
+    }
+
+    [Fact]
+    [DisplayName("Given an invalid supplier id should throw exception")]
+    public async Task GivenAnInvalidSupplierIdShouldThrowException()
+    {
+        // Given
+        CreateProductCommand command = new(1, "Test description", 20M, UnitOfMeasurement.Meter);
+        CreateProductCommandHander commandHandler = new(_productRepository, _supplierRepository);
+
+        _supplierRepository.VerifyIfExists(Arg.Any<long>())
+            .Returns(false);
+        
+        // When
+        Func<Task> result = async () => 
+            await commandHandler.Handle(command, new CancellationToken());
+
+        // Then
+        await result.Should().ThrowAsync<NotFoundException>()
+            .WithMessage("Supplier with id 1 not found");
+    }
+}
--- test/NeedleWork.UnitTests/Features/Suppliers/Queries/Suppliers/GetSupplierByIdQueryHandlerTests.cs
+++ test/NeedleWork.UnitTests/Features/Suppliers/Queries/Suppliers/GetSupplierByIdQueryHandlerTests.cs
@@ -2,7 +2,7 @@ using NeedleWork.Application.Features.Suppliers.Queries.GetById;
 using NeedleWork.Application.ViewModels.Suppliers;
 using NeedleWork.Core.Exceptions;
 
-namespace NeedleWork.UnitTests.Features.Suppliers.Queries;
+namespace NeedleWork.UnitTests.Features.Suppliers.Queries.Suppliers;
 
 public class GetSupplierByIdQueryHandlerTests
 {
@@ -26,7 +26,7 @@ public class GetSupplierByIdQueryHandlerTests
         result.Should().NotBeNull();
         result.Should().BeOfType<SupplierDetailsViewModel>();
         await _supplierRepository.Received(1).GetByIdAsync(1);
-    }   
+    }
 
     [Fact]
     [DisplayName("Given an invalid id should throw exception")]
@@ -46,5 +46,5 @@ public class GetSupplierByIdQueryHandlerTests
         // Assert
         await result.Should().ThrowAsync<NotFoundException>()
             .WithMessage("Supplier with id 1 not found");
-    } 
+    }
 }
