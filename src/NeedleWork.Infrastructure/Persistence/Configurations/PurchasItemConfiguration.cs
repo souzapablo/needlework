70ac86d2ee9c85f7dcee5c3ccd35a47271dfe19d
Pablo Souza <pablo.osouza@outlook.com>
2023-09-13 11:04:33 -0300

    feat: map new purchase related entities

---
+++ src/NeedleWork.Infrastructure/Persistence/Configurations/PurchasItemConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeedleWork.Core.Entities;
+
+namespace NeedleWork.Infrastructure.Persistence.Configurations;
+public class PurchasItemConfiguration : IEntityTypeConfiguration<PurchaseItem>
+{
+    public void Configure(EntityTypeBuilder<PurchaseItem> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasQueryFilter(x => x.IsActive);
+
+        builder.HasOne(x => x.Product)
+            .WithOne();
+    }
+}
---
+++ src/NeedleWork.Infrastructure/Persistence/Configurations/PurchaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NeedleWork.Core.Entities;
+
+namespace NeedleWork.Infrastructure.Persistence.Configurations;
+public class PurchaseConfiguration : IEntityTypeConfiguration<Purchase>
+{
+    public void Configure(EntityTypeBuilder<Purchase> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasQueryFilter(x => x.IsActive);
+    }
+}
